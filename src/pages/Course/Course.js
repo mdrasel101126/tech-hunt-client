ba2d37dc93bfe7326d67c054e1e8a8425675fbe1
mdrasel101126 <mdrasel101126@gmail.com>
2022-10-25 08:16:07 +0600

    side nav added

--- src/pages/Course/Course.js
+++ src/pages/Course/Course.js
@@ -1,10 +1,27 @@
 import React from "react";
+import { Button, Card } from "react-bootstrap";
+import { FaStar } from "react-icons/fa";
+import { Link } from "react-router-dom";
 
-const Course = () => {
+const Course = ({ course }) => {
+  console.log(course);
+  const { id, name, picture, rating, duration } = course;
   return (
-    <div>
-      <h1>Hello Course</h1>
-    </div>
+    <Card>
+      <Card.Img variant="top" src={picture} />
+      <Card.Body>
+        <Card.Title>{name} Course</Card.Title>
+        <>
+          <p>
+            Rating:<FaStar className="text-warning"></FaStar> {rating}
+          </p>
+          <p>Duration: {duration} month</p>
+        </>
+        <Link to={`/course/${id}`}>
+          <Button variant="primary">View Details</Button>
+        </Link>
+      </Card.Body>
+    </Card>
   );
 };
 
--- src/pages/Courses/Courses.js
+++ src/pages/Courses/Courses.js
@@ -1,12 +1,14 @@
 import React from "react";
 import { useLoaderData } from "react-router-dom";
+import Course from "../Course/Course";
 
 const Courses = () => {
   const courses = useLoaderData();
-  console.log(courses);
   return (
     <div>
-      <h1>Hello Courses</h1>
+      {courses.map((course) => (
+        <Course key={course.id} course={course}></Course>
+      ))}
     </div>
   );
 };
