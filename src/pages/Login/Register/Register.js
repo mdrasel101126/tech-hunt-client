9a3ecfb831a74fb66914484b55949940def10f84
mdrasel101126 <mdrasel101126@gmail.com>
2022-10-27 17:53:44 +0600

    register modified

--- src/pages/Login/Register/Register.js
+++ src/pages/Login/Register/Register.js
@@ -16,7 +16,6 @@ const Register = () => {
     const photoURL = form.photoURL.value;
     const email = form.email.value;
     const password = form.password.value;
-    console.log(name, photoURL, email, password);
     createUser(email, password)
       .then((userCredential) => {
         // Signed in
